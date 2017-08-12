b96f74c04412d96d6bb52e826eac7e60733aff26
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-08-12 18:54:58 +0200

    Add formula to get unix timestamp from natural date

--- server.js
+++ server.js
@@ -14,12 +14,15 @@ app.get('/:value', function(req, res) {
         // if it does, return the right values
         // if it doesn't, return null as values
 
+    var naturalLanguageDate;
+    var unixTimeStamp;
+    var options = { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' };
+
     if (!isNaN(req.params.value)) { // if parameter is a number then it is a valid unix timestamp
 	// get natural language date
 	// helpful: https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
-	var naturalLanguageDate = new Date(req.params.value*1000);
-	var options = { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' };
+	naturalLanguageDate = new Date(req.params.value*1000);
 	
 	res.json(
     	    {
@@ -27,11 +30,17 @@ app.get('/:value', function(req, res) {
     		natural: naturalLanguageDate.toLocaleDateString('en-US', options)
     	    }
     	);
-    } else { // if parameter is not a number then check if it is a valid natural language date
+    } else { // if parameter is not a number 
+	// check if it is a natural date in the format we want
+	// TODO
+
+		      
+	// unixTimeStamp = naturalLanguageDate.getTime()/1000;
+	
 	res.json(
     	    {
-    		unix: req.params.value,
-    		natural: '?'
+    		unix: unixTimeStamp,
+    		natural: naturalLanguageDate
     	    }
     	);
     }
