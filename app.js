e0366792b49c89e679b35f279b7e877a39134540
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-10-28 19:23:10 +0200

    Avoid sending two requests

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
+
+app.get('/', function(req, res) {
+    if(req.url === '/favicon.ico') // https://stackoverflow.com/questions/17952436/node-js-double-console-log-output
+    {
+	;
+    } else {
+	res.send('hi');
+    }
+});
 app.get('/:date', function(req, res){
-    res.send(req.params);
+    if(req.url === '/favicon.ico') // https://stackoverflow.com/questions/17952436/node-js-double-console-log-output
+    {
+	;
+    } else {
+	res.send('cool, you gave a param');
+	console.log(req.params.date); 
+    }
 });
 
 app.listen(3000, function(){
