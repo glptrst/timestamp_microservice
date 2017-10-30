ec9250e7ee2a2687f1b0c437cdf15007d3608766
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-10-30 18:57:09 +0100

    Remove useless condition

--- app.js
+++ app.js
@@ -3,29 +3,23 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    // avoid favicon request
-    if(req.url === '/favicon.ico') // https://stackoverflow.com/questions/17952436/node-js-double-console-log-output
-    {
-	console.log('Favicon requested!');
-    } else {
-	res.send('hi, give me a parameter...');
-    }
+    res.send('hi, give me a parameter...');
 });
 app.get('/:date', function(req, res){
-    // avoid favicon request
-    if(req.url === '/favicon.ico') // https://stackoverflow.com/questions/17952436/node-js-double-console-log-output
+    // avoid favicon request: https://stackoverflow.com/questions/17952436/node-js-double-console-log-output
+    if(req.url === '/favicon.ico')
     {
-	console.log('Favicon requested!');
+	;
     } else {
 	if (Number.isInteger(Number(req.params.date))) { // if parameter is an integer
 	    // if the integer is too big
 	    	// complain
 	    // Otherwise calculate date in the other format
 	    var date = new Date(Number(req.params.date)*1000);
-	    console.log(date);
+	    res.send(date);
 	} else { // if parameter is not an integer
 	    // TODO
-	    // check if it is a date in a correct format 
+	    // check if it is a date in a correct format
 	}
     }
 });
