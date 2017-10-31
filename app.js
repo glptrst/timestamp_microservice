149bf7f31156779e2a0b293486405b398414dd94
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-10-31 22:32:30 +0100

    Get natural date from unix timestamp.

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ app.get('/', function(req, res) {
     res.send('hi, give me a parameter...');
 });
 app.get('/:date', function(req, res){
+    
     // avoid favicon request: https://stackoverflow.com/questions/17952436/node-js-double-console-log-output
     if(req.url === '/favicon.ico')
     {
 	;
     } else {
 	if (Number.isInteger(Number(req.params.date))) { // if parameter is an integer
-	    // if the integer is too big
+	    // TODO
+	    // if the integer less than ? or greater than ? then it is not a valid unix timestamp
 	    	// complain
-	    // Otherwise calculate date in the other format
-	    var date = new Date(Number(req.params.date)*1000);
-	    res.send(date);
+	    // Otherwise it is a valid unix timestamp
+	    // get natural language date
+	    // helpful: https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+	    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+	    var options = { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' };
+	    var naturalLanguageDate = new Date(Number(req.params.date)*1000);
+	    res.json(
+		{
+		    unix: req.params.date,
+		    natural: naturalLanguageDate.toLocaleDateString('en-US', options)
+		}
+	    );
 	} else { // if parameter is not an integer
 	    // TODO
 	    // check if it is a date in a correct format
