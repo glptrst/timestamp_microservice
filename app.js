28785496a73b632d4e8c89de4a8952800dad2500
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-11-01 16:06:17 +0100

    Put some conditions

--- app.js
+++ app.js
@@ -6,17 +6,12 @@ app.get('/', function(req, res) {
     res.send('hi, give me a parameter...');
 });
 app.get('/:date', function(req, res){
-    
     // avoid favicon request: https://stackoverflow.com/questions/17952436/node-js-double-console-log-output
     if(req.url === '/favicon.ico')
     {
 	;
     } else {
 	if (Number.isInteger(Number(req.params.date))) { // if parameter is an integer
-	    // TODO
-	    // if the integer less than ? or greater than ? then it is not a valid unix timestamp
-	    	// complain
-	    // Otherwise it is a valid unix timestamp
 	    // get natural language date
 	    // helpful: https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
 	    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
@@ -29,8 +24,19 @@ app.get('/:date', function(req, res){
 		}
 	    );
 	} else { // if parameter is not an integer
-	    // TODO
 	    // check if it is a date in a correct format
+	    var string = req.params.date;
+	    //the parameter splitted with ' ' should give an arrary of length three
+	    var splitted = string.split(' ');
+	    if (splitted.length !== 3) {
+		res.send('invalid format!');
+	    } else {
+		// splitted[0] should be a string
+
+		// splitted[1] should be a number and a comma
+
+		// splitted[2] should be a number 
+	    }
 	}
     }
 });
